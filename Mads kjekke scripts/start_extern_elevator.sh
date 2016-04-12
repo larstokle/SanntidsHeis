daaa74568325678f82d3898c59f844f6900a9cc7
AT <god@heaven.tk>
2016-04-13 00:34:25 +0200

    Mads kjekke scripts

---
+++ Mads kjekke scripts/start_extern_elevator.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+clear
+echo "Username set to 'Student':"
+
+echo "Type in the last byte of the IP to the elvator you want to connect to:"
+read IP
+echo "Connecting to 129.241.187."$IP
+scp -rq /home/student/Desktop/sanntid18/elevateme/bin/main student@129.241.187.$IP:~/Videos/hv_kjentmann
+ssh student@129.241.187.$IP "Videos/hv_kjentmann"
+
+echo Elevator script started
+
+
